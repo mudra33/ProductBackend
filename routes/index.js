ed87d1e3be82ed318ed4e75dd654e7d714f42076
“Your <mudra.s@ahduni.edu.in>
2022-04-12 17:05:58 +0530

    added cors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+	credentials: true,
+};
+app.use(cors(corsOptions));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
